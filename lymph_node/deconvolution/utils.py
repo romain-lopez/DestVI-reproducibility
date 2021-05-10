73587ae70c7db2dfbcf55304a6f096451a696d88
romain-lopez <romain_lopez@berkeley.edu>
2021-05-10 14:21:39 +0000

    more work before release

--- lymph_node/deconvolution/utils.py
+++ lymph_node/deconvolution/utils.py
@@ -4,6 +4,7 @@ from scipy.interpolate import splrep, splev
 from scipy.spatial.distance import pdist, squareform
 import scanpy as sc
 import pandas as pd
+from sklearn.mixture import GaussianMixture
 
 
 #build the 2D colormap
@@ -135,3 +136,111 @@ def vcorrcoef(X,y):
     r_den = np.sqrt(np.sum((X-Xm)**2,axis=1)*np.sum((y-ym)**2))
     r = r_num/r_den
     return r
+
+def get_delta(lfc):
+    return np.max(np.abs(GaussianMixture(n_components=3).fit(lfc.reshape(-1, 1)).means_))
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from scipy.cluster.hierarchy import leaves_list
+
+
+def local_correlation_plot(
+            local_correlation_z, modules, linkage,
+            mod_cmap='tab10', vmin=-8, vmax=8,
+            z_cmap='RdBu_r', gene_modules={}
+):
+    """
+    Patched code from https://github.com/YosefLab/Hotspot/blob/master/hotspot/plots.py
+    """
+
+    row_colors = None
+    colors = list(plt.get_cmap(mod_cmap).colors)
+    module_colors = {i: colors[(i-1) % len(colors)] for i in modules.unique()}
+    module_colors[-1] = '#ffffff'
+
+    row_colors1 = pd.Series(
+        [module_colors[i] for i in modules],
+        index=local_correlation_z.index,
+    )
+
+    row_colors = pd.DataFrame({
+        "Modules": row_colors1,
+    })
+
+    cm = sns.clustermap(
+        local_correlation_z,
+        row_linkage=linkage,
+        col_linkage=linkage,
+        vmin=vmin,
+        vmax=vmax,
+        cmap=z_cmap,
+        xticklabels=False,
+        yticklabels=False,
+        row_colors=row_colors,
+        rasterized=True,
+    )
+    
+    fig = plt.gcf()
+    plt.sca(cm.ax_heatmap)
+    plt.ylabel("")
+    plt.xlabel("")
+
+    cm.ax_row_dendrogram.remove()
+
+    # Add 'module X' annotations
+    ii = leaves_list(linkage)
+
+    mod_reordered = modules.iloc[ii]
+
+    mod_map = {}
+    y = np.arange(modules.size)
+
+    for x in mod_reordered.unique():
+        if x == -1:
+            continue
+
+        mod_map[x] = y[mod_reordered == x].mean()
+
+    plt.sca(cm.ax_row_colors)
+    for mod, mod_y in mod_map.items():
+        plt.text(-.5, y=mod_y, s="Module {}".format(mod),
+                 horizontalalignment='right',
+                 verticalalignment='center')
+    plt.xticks([])
+
+    plt.sca(cm.ax_heatmap)
+    for mod, mod_y in mod_map.items():
+        if mod in gene_modules:
+#             plt.arrow(local_correlation_z.shape[0],mod_y,100,0, width=1) 
+#             plt.text(local_correlation_z.shape[0]+100, y=mod_y, s="\n".join(gene_modules[mod]),
+#                      horizontalalignment='left',
+#                      verticalalignment='center',
+#                      fontsize="small",
+#                      bbox= dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+            plt.annotate(text="\n".join(gene_modules[mod]), 
+                         xy=(local_correlation_z.shape[0], mod_y), 
+                         xytext=(local_correlation_z.shape[0] + 100, mod_y),
+                     arrowprops=dict(arrowstyle="-[, widthB=1.5"),
+                     horizontalalignment='left',
+                     verticalalignment='center',
+                     fontsize="small",
+                     bbox= dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+
+    # Find the colorbar 'child' and modify
+    min_delta = 1e99
+    min_aa = None
+    for aa in fig.get_children():
+        try:
+            bbox = aa.get_position()
+            delta = (0-bbox.xmin)**2 + (1-bbox.ymax)**2
+            if delta < min_delta:
+                delta = min_delta
+                min_aa = aa
+        except AttributeError:
+            pass
+
+    min_aa.set_ylabel('Z-Scores')
+    min_aa.yaxis.set_label_position("left")
